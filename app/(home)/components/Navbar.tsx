5460682f8b765441bc1eabf419627b90f7b719a8
mahfoud-mehalli <mahfoudmehalli12345@gmail.com>
2024-06-20 23:59:03 +0100

    Project done | still just the SEO

--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -1,8 +1,9 @@
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import React from 'react'
 import { SiGithub, SiInstagram, SiLinkedin } from "react-icons/si";
 
-const Navbar = () => {
+const Navbar = ({className}:{className?: string}) => {
 
     const socials = [
         {
@@ -24,7 +25,7 @@ const Navbar = () => {
 
 
     return (
-        <nav className='py-10 flex justify-between items-center'>
+        <nav className={cn('py-10 flex justify-between items-center', className)}>
             <h1 className='text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2'>mahfoudbem 💻</h1>
             <div className="flex items-center gap-5">
                 {socials.map((social, index) => {
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,10 +3,11 @@ import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
+import Footer from './components/Footer'
 
 const page = () => {
   return (
-    <div className='min-h-[400vh] bg-black overflow-hidden'>
+    <div className='min-h-screen bg-black overflow-hidden'>
       <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
 
         <div className="max-w-7xl mx-auto px-5 sm:p-5 ">
@@ -19,6 +20,7 @@ const page = () => {
       <div className='max-w-7xl mx-auto p-5 mt-20'>
         <Skills />
         <Projects />
+        <Footer />
       </div>
     </div>
   )
